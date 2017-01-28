3f8d3599eddad670ef03aaa51416d0b8875e97ee
QuoniamYIF <quoniamy@gmail.com>
2017-01-28 21:12:00 +0800

    start DL course in Udacity.it's good

---
+++ Udacity-DL/the week1/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+#read data
+datafram = pd.read_fwf('brain_body.txt')
+x_values = datafram[['Brain']]
+y_values = datafram[['Body']]
+
+#train model on data
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+#visualize results
+plt.scatter(x_values, y_values)
+plt.plot(x_values, body_reg.predict(x_values))
+plt.show()
